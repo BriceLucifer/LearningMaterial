742dca9ecc85c58bdfa204c13c00862405fe3414
BriceLucifer <2376671337@qq.com>
2024-07-22 18:28:48 +0800

    两道题解决 总结

---
+++ da/algorithm/reserveList.py
@@ -0,0 +1,15 @@
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution:
+    def reserveList(self,head:Optional[ListNode])->Optional[ListNode] :
+        if not head or not head.next:
+            return None
+
+        newhead = self.reserveList(head.next)
+        head.next.next = head
+        head.next = None
+        return newhead
+        
